a373b9b936085dc7f6d179aa7e3f2b6f29dc7545
Thiago Torres de Souza <thiagomev@gmail.com>
2024-08-07 12:35:35 -0300

    Create errors handler

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,30 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
+import { AppError } from './Errors/AppError'
+var cors = require('cors')
+
+import 'dotenv/config'
+import 'express-async-errors'
 
 const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+            message: err.message
+        });
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: `Internal server error - ${err.message}`
+    })
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("🚀 Server on port: " + process.env.PORT)
+})
 
 export { app }
